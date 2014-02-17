f0c2be7e4999891d936edba4143f64fe5452414c
Judith van Stegeren <judithvanstegeren@gmail.com>
2014-02-17 14:46:19 +0100

    Tokenizer now lexes comments and whitespace. Must still improve lexing on keywords

--- src/Tokenizer.hs
+++ src/Tokenizer.hs
@@ -2,7 +2,7 @@ module Tokenizer where
 
 import Data.List as List
 import Data.Maybe as Maybe
-import Data.Map as Map hiding (map)
+import qualified Data.Map as Map hiding (map)
 
 -- most naive implementation possible
 
@@ -16,21 +16,23 @@ data Token =
 	| Id String
 	| Boolean Bool
 	| Operator
-	| Keyword 
+	| Keyword
 	| Seperato
 	| WhiteSpace
 	deriving (Show, Eq)
 
 type Reader = (String,Int) 					--leeskop
-type Tokenlist = Maybe [Token]					--lijst van tokens of niets
+type Tokenlist = Maybe [Token]				--lijst van tokens of niets
 type Lexfun = Reader -> (Reader,Tokenlist)	--stdtype van input naar (resultaat,onbewerkte_input)
 
-keywordmap = fromList
+keywordmap = Map.fromList
 	[	("if",Keyword),
 		("then",Keyword),
 		("else",Keyword),
 		("while",Keyword)
 	]
+
+
 
 {-
 readOneToken :: Lexfun
@@ -38,6 +40,34 @@ readOneToken (input,index) =
 	let (newreader,tokenlist) = break isTokenstring input --kan niet want break :: (string -> bool) -> string -> (string,string)
 	in case tokenlist of
 -}
+
+lexComment :: Lexfun
+lexComment (input,index)
+	| isPrefixOf "//" input 		= lexLineComment (fromJust (stripPrefix "//" input)) 
+	| isPrefixOf "/*" input			= lexMultiComment (fromJust (stripPrefix "/*" input))
+	where
+		lexLineComment input = 	let	(comment,(x:rest)) = break (\x -> x == '\n') input
+								in	((rest,index+(length comment)),Just [])
+		lexMultiComment input = let (comment,rest) = splitAtEndComment [] input
+								in ((rest,index+(length comment)),Just [])
+
+splitAtEndComment :: String -> String -> (String,String)
+splitAtEndComment c [] = (c,"")
+splitAtEndComment [] _ = ("","")
+splitAtEndComment c (x:xs) = if (x == '*' && (head xs) == '/') 
+								then (c,tail xs) 
+								else splitAtEndComment (c++[x]) xs
+
+lexWhitespace :: Lexfun
+lexWhitespace (input,index) =
+	case input of 
+		('\r':xs)	-> ((xs,index+1),empty)
+		('\n':xs)	-> ((xs,index+1),empty)
+		('\t':xs)	-> ((xs,index+1),empty)
+		(' ':xs)	-> ((xs,index+1),empty)
+		_			-> ((input,index),Nothing)
+	where empty = Just []
+		
 
 lexKeyword :: Lexfun
 lexKeyword (input,index) =
