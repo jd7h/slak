8a99e785e312be25f94d0b623b5374f9fa06cc98
Judith van Stegeren <judithvanstegeren@gmail.com>
2014-03-05 15:29:59 +0100

    Draft version AST

--- src/AST.hs
+++ src/AST.hs
@@ -1,10 +1,14 @@
 module AST where
 
-type Program = [ Declaration ]
-data Declaration = V VarDecl | F FunDecl deriving (Show,Eq)
-type VarDecl = Type Id Value
-type FunDecl = Type Id [Arg] Sequence
-
-type Sequence = ( [ VarDecl ],[ Body ])
+type Program = [ Declaration ] -- a program is a list of declarations
+data Declaration = V VarDecl | F FunDecl deriving (Show,Eq) -- a declaration is either a variable decl or a function decl
+type VarDecl = Type Id Expression -- a variable declaration consists of a Type, an Id and an expression
+type FunDecl = Type Id [Arg] Sequence -- a function declaration has a Type, an Id, a (possibly empty) list of arguments and a function body
+type Sequence = ( [ VarDecl ],[ Body ]) -- a function body consists of first a list of (local variable declarations and then some statements)
 data Body = Var v | Stm s deriving (Show,Eq)
+
+
+
+
+
 
