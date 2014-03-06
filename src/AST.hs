eb7279cf62b10612e3765cb8c4f466be293f6015
Tom de Ruijter <deruijter.tom@gmail.com>
2014-03-06 17:44:49 +0100

    Compiling draft of AST.

--- src/AST.hs
+++ src/AST.hs
@@ -1,40 +1,37 @@
 module AST where
 
-type Program 		= [ Declaration ]
-data Declaration	= Var VarDecl
-			| Fun FunDecl
-type VarDecl		= Type Id Expression
-type FunDecl		= Type Id [Arg] Sequence
-type Arg		= Type Id
-type Sequence		= ([VarDecl],[Statement])
-data Statement		= Ass Id [Field] Expression			-- Variable assignment
-			| If Expression [Statement] [Statement]		-- If
-			| While Expression [Statement]			-- While
-			| Call Funcall					-- Function call
-			| Return Expression				-- Return
-data Expression		= Access Id [Field]				-- Lookup	Lijst van 0 of meer field selectors
+-- Below largely follows overall structure
+-- I've used datatypes rather than standard types to allow for flexibility in adjustments
+data Program 		= Program [Declaration]
 
-			-- DIT				-- Expressief, maar niet erg generiek. Typecheckker moet toch controleren of zo'n expressie klopt
-							-- Kan je wel foutieve arithmetische operaties niet als syntaxfout markeren
-			| BinOp Op2 Expression Expression
-			| UnOp Op1 Expression
-			-- OF WEGLATEN en als functieaanroep modelleren. 
-			
-			| Call FunCall
-			| ... -- Work in Progress
-	
-data FunCall		= Fun Id [Expression]
-			| Op Operator [Expression]	-- Deze regel komt er dan bij
-			
-data Op2		= Plus | Min | Times | Divide | Mod
-			| Eq | Lt | Gt | Le | Ge | Neq
-			| And | Or
-			| Cons
-data Op1		= Neg | Not
+data Declaration	= VarDecl Type Identifier Expression
+					| FunDecl Type Identifier [(Type, Identifier)] [Declaration] [Statement]
 
-data Operators		= ...	-- Lijst met alle operatoren
+data Type			= Void
+					| Number
+					| Boolean
 
-data Field		= Head | Tail | First | Second
-data			= Int | Bool
-type Id			= String
+data Statement		= Expression Expression					-- To support function calls
+					| Assignment Identifier [Field] Expression
+					| If Expression [Statement] [Statement]
+					| While Expression [Statement]
+					| Return (Maybe Expression)
 
+data Expression		= Var Identifier [Field]
+					| BinOp Expression Op2 Expression		-- Enables tree simplification
+					| UnOp Op1 Expression			
+					| Constant Type 
+					| FunCall Identifier [Expression]
+					| Pair Expression Expression
+					| Nil
+					
+data Field			= Head | Tail | First | Second
+					
+data Op2			= Plus | Min | Mult | Div | Mod
+					| Eq | Lt | Gt | Le | Ge | Neq
+					| And | Or
+					| Cons
+
+data Op1			= Neg | Not
+
+data Identifier		= Identifier String Int
